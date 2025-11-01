02d1dc405b0fde9a7af2a942679b182be75a26c8
André Rosa <andrer757@gmail.com>
2025-11-01 14:52:54 +0000

    logs to pp

--- Jellyfin.Plugin.PhoenixAdult/Sites/NetworkPornPros.cs
+++ Jellyfin.Plugin.PhoenixAdult/Sites/NetworkPornPros.cs
@@ -69,6 +69,9 @@ namespace PhoenixAdult.Sites
                 return await GetDataFromAPI(siteNum, searchType, newSlug, cancellationToken);
             }
 
+            Logger.Info($"[HTTP Request] status code: {req.StatusCode}");
+            Logger.Info($"[HTTP Request] content: {req.Content}");
+
             return req.IsOK ? JObject.Parse(req.Content) : null;
         }
 
