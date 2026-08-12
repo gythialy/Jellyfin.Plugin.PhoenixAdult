8636e9126af538a30e0c3e4287c9d9d4f14324b9
Goren G <gythialy.koo+github@gmail.com>
2026-08-12 11:16:28 +0800

    fix: guard Jellyfin.Data.Enums behind #if !__EMBY__ for Emby build (#85)
    * fix: guard Jellyfin.Data.Enums behind #if !__EMBY__ for Emby build
    
    - Wrap using Jellyfin.Data.Enums in #if !__EMBY__ in 161 site handlers
    - Add GlobalUsings.cs alias: PersonKind -> MediaBrowser.Model.Entities.PersonType under __EMBY__
    - Remove unused Jellyfin.Data.Enums usings in 4 handlers
    
    Verified: Release.Emby and Release both build with 0 warnings, 0 errors
    
    * ci: build Emby config in PR CI; make online smoke manual
    
    - Add Release.Emby build step so Emby regressions are caught in PRs
      (nightly/release workflows build it, but ci.yml did not)
    - Change online-smoke to workflow_dispatch only (manual trigger)

--- Jellyfin.Plugin.PhoenixAdult/Sites/SiteArchAngel.cs
+++ Jellyfin.Plugin.PhoenixAdult/Sites/SiteArchAngel.cs
@@ -6,7 +6,9 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
+#if !__EMBY__
 using Jellyfin.Data.Enums;
+#endif
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Providers;
